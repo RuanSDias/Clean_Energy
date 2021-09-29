0d6bb02e3a40068eadfe2ba7cea0a4aae9b114c8
Ruan <sdias.ruan@gmail.com>
2021-09-29 10:59:31 -0300

    [Feature Dias - Task: Creating initial Spring project]

---
+++ cleanEnergy/src/main/java/br/com/project/cleanEnergy/CleanEnergyApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.cleanEnergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanEnergyApplication.class, args);
+	}
+
+}
---
+++ cleanEnergy/src/test/java/br/com/project/cleanEnergy/CleanEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.cleanEnergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
