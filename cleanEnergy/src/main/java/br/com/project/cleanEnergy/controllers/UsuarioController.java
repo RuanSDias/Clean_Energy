40f84fdbe57ba1164cc8739a2105fa24e3146387
heiitalo <italovieira.008@gmail.com>
2021-10-13 12:13:58 -0300

    [Feature Task Italo: created method logar]

--- cleanEnergy/src/main/java/br/com/project/cleanEnergy/controllers/UsuarioController.java
+++ cleanEnergy/src/main/java/br/com/project/cleanEnergy/controllers/UsuarioController.java
@@ -5,19 +5,26 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 import br.com.project.cleanEnergy.Repository.UsuarioRepository;
 import br.com.project.cleanEnergy.model.UsuarioModel;
+import br.com.project.cleanEnergy.model.dtos.CredenciaisDTO;
+import br.com.project.cleanEnergy.model.dtos.UsuarioLoginDTO;
 import br.com.project.cleanEnergy.service.UsuarioService;
 
 @RestController
@@ -52,8 +59,12 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(400).build());
 
 	}
-
-
+	
+	@PutMapping("/logar")
+	public ResponseEntity<CredenciaisDTO> credenciais(@Valid @RequestBody UsuarioLoginDTO usuarioParaAutenticar) {
+		return servicos.pegarCredenciais(usuarioParaAutenticar);
+	}
+	
 	@DeleteMapping("/deletar/{id_usuario}")
 	public ResponseEntity<UsuarioModel> deletar(@PathVariable(value = "id_usuario") Long idUsuario) {
 		Optional<UsuarioModel> objetoOptional = repository.findById(idUsuario);
