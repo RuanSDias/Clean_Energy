35c35ee3ec461a467d42b0844d5e212a38a9a03f
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2021-10-19 12:05:31 -0300

    [Feature Patrícia - Task: Add method PUT user]

--- cleanEnergy/src/main/java/br/com/project/cleanEnergy/controllers/UsuarioController.java
+++ cleanEnergy/src/main/java/br/com/project/cleanEnergy/controllers/UsuarioController.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -88,6 +90,15 @@ public class UsuarioController {
 	public ResponseEntity<CredenciaisDTO> credenciais(@Valid @RequestBody UsuarioLoginDTO usuarioParaAutenticar) {
 		return servicos.pegarCredenciais(usuarioParaAutenticar);
 	}
+	
+	@ApiOperation(value = "Atualiza usuário existente")
+	@ApiResponses(value = {
+	@ApiResponse(code = 201, message = "Retorna usuário atualizado")})
+	@PutMapping
+	public ResponseEntity<UsuarioModel> atualizar(@Valid @RequestBody UsuarioModel usuario){
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(repository.save(usuario));
+   }
 	
 	@ApiOperation(value = "Deletar usuário existente")
 	@ApiResponses(value = {
