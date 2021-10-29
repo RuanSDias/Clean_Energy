09e2a40501b94d884110e76af50a54e04fa2badc
Ruan <sdias.ruan@gmail.com>
2021-10-29 14:55:14 -0300

    [Feature Dias - Task: Configure BasicSecurity policies.]

--- src/main/java/br/com/project/cleanEnergy/security/BasicSecurityConfig.java
+++ src/main/java/br/com/project/cleanEnergy/security/BasicSecurityConfig.java
@@ -13,35 +13,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	private @Autowired UserDetailsServiceImplements service;
 
 	@Bean
-	public PasswordEncoder senhaEncoder () {
+	public PasswordEncoder senhaEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	http.authorizeRequests()
-	.antMatchers("/usuarios/cadastrar").permitAll()
-	.antMatchers("/usuarios/logar").permitAll()
-	.antMatchers(HttpMethod.OPTIONS).permitAll()
-	.anyRequest().authenticated()
-	.and().httpBasic()
-	.and().sessionManagement()
-	.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	.and().cors()
-	.and().csrf().disable();
+		http.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/v1/usuario/salvar").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/v1/usuario/logar").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/usuario").permitAll()
+				.antMatchers(HttpMethod.GET, "/produtos").permitAll()
+			.anyRequest().authenticated()
+			.and().httpBasic()
+			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().cors()
+			.and().csrf().disable();
 	}
-	
+
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-	throws Exception {
-	auth.userDetailsService(service);
-	auth.inMemoryAuthentication()
-	.withUser("root").password(senhaEncoder().encode("root")).authorities("ROLE_USER");
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service);
+		auth.inMemoryAuthentication().withUser("root").password(senhaEncoder().encode("root")).authorities("ROLE_ADMIN");
 	}
-	
-	
+
 }
